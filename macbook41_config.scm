367388451930a9c4528312fccbc98757b7dd98a5
Efraim Flashner <efraim@flashner.co.il>
2017-10-23 19:48:34 +0300

    fix tabs in config

--- macbook41_config.scm
+++ macbook41_config.scm
@@ -12,9 +12,6 @@
   (bootloader (bootloader-configuration
                 (bootloader grub-efi-bootloader)
                 (target "/boot/efi")))
-  ;(bootloader (grub-configuration
-  ;              (grub grub-efi)
-  ;              (device "/dev/sda1")))
 
   (kernel-arguments '("zswap.enabled=1" "zram.max_pool_percent=50"))
 
@@ -22,18 +19,18 @@
                          (device "my-root")
                          (mount-point "/")
                          (type "btrfs")
-			 (title 'label)
-			 (options "autodefrag,compress=lzo"))
-		       (file-system
-			 (device "none")
-			 (mount-point "/var/guix/temproots")
-			 (title 'device)
-			 (type "tmpfs")
-			 (check? #f))
-		       (file-system
-			(device "/dev/sda1")
-			(mount-point "/boot/efi")
-			(type "vfat"))
+                         (title 'label)
+                         (options "autodefrag,compress=lzo"))
+                       (file-system
+                         (device "none")
+                         (mount-point "/var/guix/temproots")
+                         (title 'device)
+                         (type "tmpfs")
+                         (check? #f))
+                       (file-system
+                         (device "/dev/sda1")
+                         (mount-point "/boot/efi")
+                         (type "vfat"))
                        %base-file-systems))
 
   (swap-devices '("/dev/sda2"))
@@ -50,7 +47,7 @@
   ;; This is where we specify system-wide packages.
   (packages (cons* nss-certs         ;for HTTPS access
                    gvfs              ;for user mounts
-		   btrfs-progs
+                   btrfs-progs
                    %base-packages))
 
   ;; Add GNOME and/or Xfce---we can choose at the log-in
@@ -58,29 +55,29 @@
   ;; include the X11 log-in service, networking with Wicd,
   ;; and more.
   (services (cons* (xfce-desktop-service)
-		   (service guix-publish-service-type
-			    (guix-publish-configuration
-			      (port 3000)))
-		   (service openssh-service-type
-			    (openssh-configuration
-			      (port-number 22)
-			      (allow-empty-passwords? #f)
-			      (password-authentication? #t)))
-		   (tor-service)
+                   (service guix-publish-service-type
+                            (guix-publish-configuration
+                              (port 3000)))
+                   (service openssh-service-type
+                            (openssh-configuration
+                              (port-number 22)
+                              (allow-empty-passwords? #f)
+                              (password-authentication? #t)))
+                   (tor-service)
                    (modify-services %desktop-services
-		     (guix-service-type config =>
-					(guix-configuration
-					  (inherit config)
-					  (substitute-urls
-					    (cons* "https://bayfront.guixsd.org"
-						   "https://berlin.guixsd.org"
-						   "http://192.168.1.134:8181" ; odroid-c2
-						   "http://192.168.1.183" ; E1240
-						   %default-substitute-urls))))
-		     (ntp-service-type config =>
-				       (ntp-configuration
-					 (inherit config)
-					 (allow-large-adjustment? #t))))))
+                     (guix-service-type config =>
+                                        (guix-configuration
+                                          (inherit config)
+                                          (substitute-urls
+                                            (cons* "https://bayfront.guixsd.org"
+                                                   "https://berlin.guixsd.org"
+                                                   "http://192.168.1.134:8181" ; odroid-c2
+                                                   "http://192.168.1.183" ; E1240
+                                                   %default-substitute-urls))))
+                     (ntp-service-type config =>
+                                       (ntp-configuration
+                                         (inherit config)
+                                         (allow-large-adjustment? #t))))))
 
   ;; Allow resolution of '.local' host names with mDNS.
   (name-service-switch %mdns-host-lookup-nss))
