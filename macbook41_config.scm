e04e9e6ab79200dec2496e73503d0f5b229db549
Efraim Flashner <efraim@flashner.co.il>
2017-11-18 18:54:18 +0200

    Revert "macbook41 config: Try different module for blacklist"
    This also failed to fix the pointer issue.
    
    This reverts commit 6420eedfc78155515f227486a5139a654295d29d.

--- macbook41_config.scm
+++ macbook41_config.scm
@@ -17,7 +17,7 @@
                 (bootloader grub-efi-bootloader)
                 (target "/boot/efi")))
 
-  (kernel-arguments '("zswap.enabled=1" "modprobe.blacklist=appletouch"))
+  (kernel-arguments '("zswap.enabled=1"))
 
   (file-systems (cons* (file-system
                          (device "my-root")
