d9357ff9ecf87b5816181ddaa170ff20b8235a63
Efraim Flashner <efraim@flashner.co.il>
2018-12-08 19:46:00 +0200

    E2140: Fix typo

--- E2140_guix_config.scm
+++ E2140_guix_config.scm
@@ -108,7 +108,7 @@
                                                   "https://bayfront.guixsd.org"
                                                   ;"http://192.168.1.134:8181" ; odroid-c2
                                                   "http://192.168.1.209:3000" ; macbook41
-                                                  "https://mirror.hydra.gnu.org"))
+                                                  "https://mirror.hydra.gnu.org"))))
                      (ntp-service-type config =>
                                        (ntp-configuration
                                          (inherit config)
