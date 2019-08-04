e95d4902f4dcda00c4fd900dc64100c8b1d78a73
Efraim Flashner <efraim@flashner.co.il>
2019-08-04 10:25:37 +0300

    macbook41: figure out var-cache-fontconfig later

--- macbook41_config.scm
+++ macbook41_config.scm
@@ -76,12 +76,12 @@
                          (type "tmpfs")
                          (check? #f))
                        ;; This directory shouldn't exist
-                       (file-system
-                         (device "none")
-                         (mount-point "/var/cache/fontconfig")
-                         (type "tmpfs")
-                         (flags '(read-only))
-                         (check? #f))
+                       ;(file-system
+                       ;  (device "none")
+                       ;  (mount-point "/var/cache/fontconfig")
+                       ;  (type "tmpfs")
+                       ;  (flags '(read-only))
+                       ;  (check? #f))
                        (file-system
                          (device (uuid "F010-1913" 'fat))
                          (mount-point "/boot/efi")
