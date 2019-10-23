9d0e9d2b682bbda814884f13abbc918e2ae7178f
Efraim Flashner <efraim@flashner.co.il>
2019-10-23 11:40:14 +0300

    tobias' substitute server is offline

--- E5400_config.scm
+++ E5400_config.scm
@@ -168,11 +168,12 @@
                                          "https://ci.guix.gnu.org"
                                          "https://bayfront.guixsd.org"
                                          "http://guix.genenetwork.org"
-                                         "https://guix.tobias.gr"))
+                                         ;"https://guix.tobias.gr"
+                                         ))
                                  (authorized-keys
-                                   (list (local-file "Extras/ci.guix.gnu.org.pub")
-                                         (local-file "Extras/E2140_publish.pub")
+                                   (list (local-file "Extras/E2140_publish.pub")
                                          (local-file "Extras/macbook41_publish.pub")
+                                         (local-file "Extras/ci.guix.gnu.org.pub")
                                          (local-file "Extras/guix.genenetwork.org.pub")
                                          (local-file "Extras/guix.tobias.gr.pub")))
                                  (extra-options
