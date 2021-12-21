c993df6b81f3e222cbb19d904ba9262c0c9bdb91
Efraim Flashner <efraim@flashner.co.il>
2021-12-21 08:49:38 +0200

    add pinebookpro key

--- config/guix-daemon.scm
+++ config/guix-daemon.scm
@@ -17,6 +17,7 @@
 (define %authorized-keys
   (list (local-file "../Extras/3900XT_publish.pub")
         (local-file "../Extras/E5400_publish.pub")
+        (local-file "../Extras/pinebookpro_publish.pub")
         (local-file "../Extras/pine64_publish.pub")
         (local-file "../Extras/g4_publish.pub")
         (local-file "../Extras/unmatched_publish.pub")
