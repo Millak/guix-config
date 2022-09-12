3640462f5e3a2e3820e0fd1ec026058e2d5af41b
Efraim Flashner <efraim@flashner.co.il>
2022-09-12 14:41:03 +0300

    Add starfive visionfive1 key

--- config/guix-daemon.scm
+++ config/guix-daemon.scm
@@ -25,6 +25,7 @@
         (local-file "../Extras/pine64_publish.pub")
         (local-file "../Extras/g4_publish.pub")
         (local-file "../Extras/unmatched_publish.pub")
+        (local-file "../Extras/starfive-vision1.pub")
         (local-file "../Extras/ci.guix.gnu.org.pub")
         (local-file "../Extras/bayfront.guix.gnu.org.pub")
         (local-file "../Extras/bordeaux.guix.gnu.org.pub")
