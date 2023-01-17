3637e759d5e759266b0ca2002eaba786d841d753
Efraim Flashner <efraim@flashner.co.il>
2023-01-17 09:51:11 +0200

    gparted: Massive custom package updates.

--- gparted.scm
+++ gparted.scm
@@ -13,39 +13,20 @@
   admin
   xorg)
 (use-package-modules
+  compression
   disk
+  gl
   gtk
   linux
+  llvm
   package-management
   wm
   xorg)
 
 ;;
 
-(define fluxbox-custom
-  (package
-    (name "fluxbox-custom")
-    (version (package-version fluxbox))
-    (source #f)
-    (build-system trivial-build-system)
-    (arguments
-     `(#:modules ((guix build utils))
-       #:builder
-       (begin (use-modules (guix build utils))
-              (let* ((source (assoc-ref %build-inputs "fluxbox"))
-                     (out    (assoc-ref %outputs "out")))
-                (copy-recursively source out)
-                (substitute* (string-append out "/share/fluxbox/menu")
-                  (("\\(firefox.*") "(gparted) {gparted}\n"))))))
-    (native-inputs (list fluxbox))
-    (home-page (package-home-page fluxbox))
-    (synopsis (package-synopsis fluxbox))
-    (description (package-description fluxbox))
-    (license (package-license fluxbox))))
-
 (define gtk+-minimal
-  (package
-    (inherit gtk+)
+  (package/inherit gtk+
     (arguments
      (substitute-keyword-arguments (package-arguments gtk+)
        ;; The tests need more of the inputs that we've stripped
@@ -55,7 +36,13 @@
         `(list "--disable-cups"
                "--disable-introspection"
                "--enable-x11-backend"
-               "--disable-gtk-doc-html"))))
+               "--disable-gtk-doc-html"
+               (string-append "--with-html-dir="
+                              (assoc-ref %outputs "doc")
+                              "/share/gtk-doc/html")
+               (string-append "--localedir="
+                              (assoc-ref %outputs "doc")
+                              "/share/locale")))))
     (propagated-inputs
      (modify-inputs (package-propagated-inputs gtk+)
                     (prepend gdk-pixbuf)
@@ -81,28 +68,139 @@
                     (delete "colord-minimal"
                             "cups"
                             "graphene"
+                            "iso-codes"
                             "harfbuzz")))))
 
 (define harfbuzz-minimal
-  (package
-    (inherit harfbuzz)
+  (package/inherit harfbuzz
+    (outputs (cons "doc" (package-outputs harfbuzz)))
     (arguments
      (substitute-keyword-arguments (package-arguments harfbuzz)
        ((#:configure-flags cf ''())
         `(cons* "--with-icu=no"
+                "--disable-introspection"
+                "--disable-gtk-doc-html"
+                (string-append "--with-html-dir="
+                               (assoc-ref %outputs "doc")
+                               "/share/gtk-doc/html")
                 (delete "--with-graphite2" ,cf)))))
     (propagated-inputs
      (modify-inputs (package-propagated-inputs harfbuzz)
                     (delete "graphite2" "icu4c")))))
 
-(define use-minimal-gtk+
+(define util-linux-minimal
+  (package/inherit util-linux
+    (arguments
+     (substitute-keyword-arguments (package-arguments util-linux)
+       ((#:phases phases)
+        `(modify-phases ,phases
+           (add-after 'install 'remove-localizations
+             (lambda* (#:key outputs #:allow-other-keys)
+               (delete-file-recursively
+                 ;; ~75% of the "lib" output.
+                 (string-append (assoc-ref outputs "lib")
+                                "/share/locale"))))))))))
+
+(define mesa-smaller
+  (package/inherit mesa
+    (arguments
+     (substitute-keyword-arguments (package-arguments mesa)
+       ((#:modules modules)
+        `((ice-9 regex)
+          (srfi srfi-26)
+          ,@modules))
+       ((#:build-type _) "minsize") ; decreases the size by ~30%.
+       ((#:configure-flags cf ''())
+        `(append
+           (remove
+             (cut string-match
+                  "-D(platforms|vulkan-(drivers|layers)|gles2|gbm|shared-glapi)=.*" <>)
+             ,cf)
+
+           ;; This has to go last so we can disable vulkan.
+           (list "-Dplatforms=x11"
+                 "-Dvulkan-drivers=")))))
+    (inputs
+     (modify-inputs (package-inputs mesa)
+                    (prepend (list zstd "lib"))
+                    ;; TODO: Can this be taken care of with use-minimized-inputs?
+                    (replace "llvm" llvm-minimal)
+                    (delete "wayland" "wayland-protocols")))))
+
+;; We could use a newer version of llvm, but this is the version mesa
+;; is currently built against, so it has the most testing in Guix.
+(define llvm-minimal
+  (package/inherit llvm-11
+    ;; If we can separate out the include directory we'd save another 21MB.
+    (outputs (list "out"))
+    (arguments
+     (substitute-keyword-arguments (package-arguments llvm-11)
+       ((#:build-type _) "MinSizeRel") ; decreases the size by ~25%
+       ((#:configure-flags cf ''())
+        ;; AMDGPU is needed by the vulkan drivers.
+        `(list ,(string-append "-DLLVM_TARGETS_TO_BUILD="
+                                (system->llvm-target) ";AMDGPU")
+               "-DLLVM_BUILD_TOOLS=NO"
+               "-DLLVM_BUILD_LLVM_DYLIB=YES"
+               "-DLLVM_LINK_LLVM_DYLIB=YES"))
+       ((#:phases phases)
+        `(modify-phases ,phases
+           (add-after 'install 'delete-static-libraries
+             (lambda* (#:key outputs #:allow-other-keys)
+               (for-each delete-file
+                         (find-files (string-append
+                                       (assoc-ref outputs "out") "/lib")
+                                     "\\.a$"))))
+           (replace 'install-opt-viewer
+             (lambda* (#:key outputs #:allow-other-keys)
+               (let ((out (assoc-ref outputs "out")))
+                 (delete-file-recursively
+                   (string-append out "/share/opt-viewer")))))
+           (add-after 'install 'build-and-install-llvm-config
+             (lambda* (#:key outputs #:allow-other-keys)
+               (let ((out (assoc-ref outputs "out")))
+                 (substitute*
+                   "tools/llvm-config/CMakeFiles/llvm-config.dir/link.txt"
+                   (("/tmp/guix-build-llvm-11.0.0.drv-0/build/lib")
+                    (string-append out "/lib")))
+                 (invoke "make" "llvm-config")
+                 (install-file "bin/llvm-config"
+                               (string-append out "/bin")))))))))))
+
+(define use-minimized-inputs
   (package-input-rewriting/spec
     `(("gtk+" . ,(const gtk+-minimal))
-      ("harfbuzz" . ,(const harfbuzz-minimal)))))
+      ("harfbuzz" . ,(const harfbuzz-minimal))
+      ("llvm" . ,(const llvm-minimal))
+      ("mesa" . ,(const mesa-smaller))
+      ("util-linux" . ,(const util-linux-minimal)))))
+
+;;
+
+(define fluxbox-custom
+  (package
+    (name "fluxbox-custom")
+    (version (package-version fluxbox))
+    (source #f)
+    (build-system trivial-build-system)
+    (arguments
+     `(#:modules ((guix build utils))
+       #:builder
+       (begin (use-modules (guix build utils))
+              (let ((source (assoc-ref %build-inputs "fluxbox"))
+                    (out    (assoc-ref %outputs "out")))
+                (copy-recursively source out)
+                (substitute* (string-append out "/share/fluxbox/menu")
+                  (("\\(firefox.*") "(gparted) {gparted}\n"))))))
+    (native-inputs (list (use-minimized-inputs fluxbox)))
+    (home-page (package-home-page fluxbox))
+    (synopsis (package-synopsis fluxbox))
+    (description (package-description fluxbox))
+    (license (package-license fluxbox))))
 
 (define gparted-custom
   (package
-    (inherit (use-minimal-gtk+ gparted))))
+    (inherit (use-minimized-inputs gparted))))
 
 ;;
 
