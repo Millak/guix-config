169f17745318b88cbd233951289bee9e3d7914bf
Efraim Flashner <efraim@flashner.co.il>
2026-04-09 16:31:03 +0300

    Update aarch64 deploy

--- aarch64-deploy.scm
+++ aarch64-deploy.scm
@@ -9,24 +9,24 @@
         (environment managed-host-environment-type)
         (configuration (machine-ssh-configuration
                          (host-name "pine64.unicorn-typhon.ts.net")
-                         ;(host-name "192.168.68.51")
+                         ;(host-name "192.168.68.54")
                          (system "aarch64-linux")
                          (port 22)
                          (user "efraim")
                          (identity "/home/efraim/.ssh/id_ecdsa")
-                         (host-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAhNEOFzg4QMgRzivcJwHQHhbVY0AAHwx9l+65wDMO6X"))))
+                         (host-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAICJCga76AhIpOHWxIs01aC5fDdnFGf9tF6NLUFe0+4w4")))))
 
       (machine
         (operating-system %rock64-system)
         (environment managed-host-environment-type)
         (configuration (machine-ssh-configuration
                          (host-name "rock64.unicorn-typhon.ts.net")
-                         ;(host-name "192.168.68.56")
+                         ;(host-name "192.168.68.68")
                          (system "aarch64-linux")
                          (port 22)
                          (user "efraim")
                          (identity "/home/efraim/.ssh/id_ecdsa")
-                         (host-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIM46O8629shIIF9WBtr/m7MvFdI5H9jR0PVFGJIcrvdm")))))
+                         (host-key "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFGUov7RxBLAvJeeRh0Yyp/mpScKCqJcnrCaesv1GAr2")))))
 
 ;; For /etc/passwd
 ;; efraim ALL = NOPASSWD: ALL
