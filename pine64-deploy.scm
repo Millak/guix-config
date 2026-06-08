5ef195ec407eaeb990bac40d979ad19114c36f88
Efraim Flashner <efraim@flashner.co.il>
2026-06-08 11:48:04 +0300

    Update pine64 IP address

--- pine64-deploy.scm
+++ pine64-deploy.scm
@@ -8,7 +8,7 @@
         (environment managed-host-environment-type)
         (configuration (machine-ssh-configuration
                          (host-name "pine64.unicorn-typhon.ts.net")
-                         ;(host-name "192.168.68.54")
+                         ;(host-name "192.168.68.59")
                          (system "aarch64-linux")
                          (port 22)
                          (user "efraim")
